cfdf5fcc1cc6345046ae5584cc7fcdc149b6fc9f
vvpreo <vvpreo@gmail.com>
2024-09-06 13:17:06 +0700

    some fixes

--- src/devbooster/__init__.py
+++ src/devbooster/__init__.py
@@ -1,11 +1,10 @@
-from . import common
-from common import BaseLogging
+from .common import BaseLogging
 
-from common import interrupt_handler
+from .common import interrupt_handler
 
 InterruptHandler = interrupt_handler.InterruptHandler
 
 BaseLogging = BaseLogging.BaseLogging
-from common import MetaSingleton
+from .common import MetaSingleton
 
 MetaSingleton = MetaSingleton.MetaSingleton
